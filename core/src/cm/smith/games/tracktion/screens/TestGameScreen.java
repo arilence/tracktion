83e43fde394ee83c09e37cafea5799a73a450126
Anthony Smith <anthony@smith.cm>
2016-11-10 01:33:53 -0800

    Add basic hud for controlling vehicle

--- core/src/cm/smith/games/tracktion/screens/TestGameScreen.java
+++ core/src/cm/smith/games/tracktion/screens/TestGameScreen.java
@@ -1,11 +1,18 @@
 package cm.smith.games.tracktion.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import cm.smith.games.tracktion.MainGame;
 import cm.smith.games.tracktion.controllers.GameController;
 import cm.smith.games.tracktion.ui.LabelButton;
+import cm.smith.games.tracktion.ui.UIImageButton;
 
 /**
  * Created by anthony on 2016-11-03.
@@ -15,6 +22,10 @@ public class TestGameScreen extends BaseScreen {
 
     private GameController gameController;
     private Vector3 touchPoint;
+
+    private boolean leftDown = false;
+    private boolean rightDown = false;
+    private boolean accelerateDown = false;
 
     public TestGameScreen(MainGame game, GameController.ROLE role) {
         super(game);
@@ -29,6 +40,69 @@ public class TestGameScreen extends BaseScreen {
         // TODO: setup on-screen hud
         //      if driver: (speedometer, left/right control buttons, pause/leave button)
         //      if builder: (next piece, map grid to place on)
+
+        Texture texture = game.assetManager.get("gamecontrols.png", Texture.class);
+        TextureRegion leftTexture = new TextureRegion(texture, 0, 0, 64, 128);
+        TextureRegion rightTexture = new TextureRegion(texture, 0, 0, 64, 128);
+        TextureRegion accelerateTexture = new TextureRegion(texture, 0, 0, 128, 64);
+        rightTexture.flip(true, false);
+        UIImageButton turnLeftButton = UIImageButton.makeButton(this.game, leftTexture);
+        UIImageButton turnRightButton = UIImageButton.makeButton(this.game, rightTexture);
+        UIImageButton accelerateButton = UIImageButton.makeButton(this.game, accelerateTexture);
+
+        turnLeftButton.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                leftDown = true;
+                return super.touchDown(event, x, y, pointer, button);
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                leftDown = false;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        turnRightButton.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                rightDown = true;
+                return super.touchDown(event, x, y, pointer, button);
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                rightDown = false;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        accelerateButton.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                accelerateDown = true;
+                return super.touchDown(event, x, y, pointer, button);
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                accelerateDown = false;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        Table turningTable = new Table();
+        turningTable.setFillParent(true);
+        turningTable.add(turnLeftButton).padLeft(80 * SCALE_X).padBottom(70 * SCALE_Y);
+        turningTable.add(turnRightButton).padLeft(100 * SCALE_X).padBottom(70 * SCALE_Y);
+
+        Table gearbox = new Table();
+        gearbox.setFillParent(true);
+        gearbox.add(accelerateButton.padRight(80 * SCALE_X).padBottom(70 * SCALE_Y));
+
+        Stack stack = new Stack();
+        stack.setFillParent(true);
+
+        stack.add(turningTable.bottom().left());
+        stack.add(gearbox.bottom().right());
+
+        uiStage.addActor(stack);
     }
 
     @Override
@@ -40,18 +114,28 @@ public class TestGameScreen extends BaseScreen {
     public void render(float delta) {
         super.render(delta);
 
-        if (Gdx.input.isTouched()) {
-            this.camera.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+//        if (Gdx.input.isTouched()) {
+//            this.camera.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+//
+//            float midpoint = MainGame.VIEW_WIDTH / 2;
+//            if (touchPoint.x < midpoint) {
+//                // TODO: tell vehicle to turn left
+//                Gdx.app.log("VEHICLE", "LEFT, " + Float.toString(midpoint) + ", " + Float.toString(touchPoint.x));
+//            }
+//            if (touchPoint.x >= midpoint){
+//                // TODO: tell vehicle to turn right
+//                Gdx.app.log("VEHICLE", "RIGHT, " + Float.toString(midpoint) + ", " + Float.toString(touchPoint.x));
+//            }
+//        }
 
-            float midpoint = MainGame.VIEW_WIDTH / 2;
-            if (touchPoint.x < midpoint) {
-                // TODO: tell vehicle to turn left
-                Gdx.app.log("VEHICLE", "LEFT, " + Float.toString(midpoint) + ", " + Float.toString(touchPoint.x));
-            }
-            if (touchPoint.x >= midpoint){
-                // TODO: tell vehicle to turn right
-                Gdx.app.log("VEHICLE", "RIGHT, " + Float.toString(midpoint) + ", " + Float.toString(touchPoint.x));
-            }
+        if (leftDown) {
+            Gdx.app.log("VEHICLE", "LEFT");
+        }
+        if (rightDown) {
+            Gdx.app.log("VEHICLE", "RIGHT");
+        }
+        if (accelerateDown) {
+            Gdx.app.log("VEHICLE", "ACCELERATE");
         }
 
     }
---
+++ core/src/cm/smith/games/tracktion/ui/UIImageButton.java
@@ -0,0 +1,101 @@
+package cm.smith.games.tracktion.ui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import aurelienribon.tweenengine.TweenAccessor;
+import cm.smith.games.tracktion.MainGame;
+import cm.smith.games.tracktion.Tweens;
+import cm.smith.games.tracktion.screens.BaseScreen;
+
+/**
+ * Created by anthony on 2016-11-09.
+ */
+
+public class UIImageButton extends ImageButton implements TweenAccessor<ImageButton> {
+
+    private UIImageButton(ImageButtonStyle style) {
+        super(style);
+    }
+
+    /**
+     * Method that generates a text button with a custom callback function
+     * @param texture Image that is displayed on the button
+     * @param callback Method is called once the button is clicked
+     * @return
+     */
+    public static UIImageButton makeButton(MainGame game, TextureRegion texture) {
+        ImageButton.ImageButtonStyle btnStyle = new ImageButton.ImageButtonStyle();
+        //btnStyle.up = new TextureRegionDrawable(Assets.btnCircleUpState);
+        //btnStyle.down = new TextureRegionDrawable(Assets.btnCircleDownState);
+        btnStyle.imageUp = new TextureRegionDrawable(texture);
+        btnStyle.pressedOffsetX = -1;
+        btnStyle.unpressedOffsetX = -1;
+
+        UIImageButton tempBtn = new UIImageButton(btnStyle);
+        return tempBtn;
+    }
+
+    /**
+     * Change the alpha of the LabelButton to either 100% or 0%
+     * @param isInvisible whether or not the LabelButton is visible or not
+     */
+    public void setInvisible(boolean isInvisible) {
+        Color tempColor = getColor();
+        tempColor.a = (isInvisible) ? 0 : 1;
+        setColor(tempColor);
+    }
+
+    @Override
+    public int getValues(ImageButton target, int tweenType, float[] returnValues) {
+        switch (tweenType) {
+            case Tweens.POSITION_X: returnValues[0] = target.getX(); return 1;
+            case Tweens.POSITION_Y: returnValues[0] = target.getY(); return 1;
+            case Tweens.POSITION_XY:
+                returnValues[0] = target.getX();
+                returnValues[1] = target.getY();
+                return 2;
+            case Tweens.ALPHA:
+                returnValues[0] = target.getColor().a;
+                return 1;
+            default: assert false; return -1;
+        }
+    }
+
+    @Override
+    public void setValues(ImageButton target, int tweenType, float[] newValues) {
+        switch (tweenType) {
+            case Tweens.POSITION_X: target.setX(newValues[0]); break;
+            case Tweens.POSITION_Y: target.setY(newValues[0]); break;
+            case Tweens.POSITION_XY:
+                target.setX(newValues[0]);
+                target.setY(newValues[1]);
+                break;
+            case Tweens.ALPHA:
+                Color oldColor = target.getColor();
+                oldColor.a = newValues[0];
+                target.setColor(oldColor);
+                break;
+            default: assert false; break;
+        }
+    }
+
+    /**
+     * The Callback interface.
+     *
+     * @author Anthony Smith
+     */
+    public interface Callback {
+        /**
+         * To do when button is clicked.
+         */
+        void onClick();
+    }
+
+}
