dcd19b96fd5c932c4e0549caa0d27aaa58c2a413
Anthony Smith <anthony@smith.cm>
2016-12-08 02:15:20 -0800

    Fix font artifacts on high dpi screens

--- core/src/cm/smith/games/tracktion/screens/LoadingScreen.java
+++ core/src/cm/smith/games/tracktion/screens/LoadingScreen.java
@@ -58,30 +58,31 @@ public class LoadingScreen extends BaseScreen {
     private void loadFonts() {
         // Setup the font loader
         FileHandleResolver resolver = new InternalFileHandleResolver();
+        FreeTypeFontGenerator.setMaxTextureSize(2048);
         game.assetManager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(resolver));
         game.assetManager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(resolver));
 
         FreetypeFontLoader.FreeTypeFontLoaderParameter parameter30 = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
         parameter30.fontFileName = "Roboto-Light.ttf";
-        parameter30.fontParameters.size = (int)(30 * (BaseScreen.SCALE_X));
+        parameter30.fontParameters.size = Math.round(30 * (BaseScreen.SCALE_X));
         parameter30.fontParameters.color = Color.WHITE;
         game.assetManager.load("font30.ttf", BitmapFont.class, parameter30);
 
         FreetypeFontLoader.FreeTypeFontLoaderParameter parameter40 = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
         parameter40.fontFileName = "Roboto-Light.ttf";
-        parameter40.fontParameters.size = (int)(40 * (BaseScreen.SCALE_X));
+        parameter40.fontParameters.size = Math.round(40 * (BaseScreen.SCALE_X));
         parameter40.fontParameters.color = Color.WHITE;
         game.assetManager.load("font40.ttf", BitmapFont.class, parameter40);
 
         FreetypeFontLoader.FreeTypeFontLoaderParameter parameter75 = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
         parameter75.fontFileName = "Roboto-Light.ttf";
-        parameter75.fontParameters.size = (int)(75 * (BaseScreen.SCALE_X));
+        parameter75.fontParameters.size = Math.round(75 * (BaseScreen.SCALE_X));
         parameter75.fontParameters.color = Color.WHITE;
         game.assetManager.load("font75.ttf", BitmapFont.class, parameter75);
 
         FreetypeFontLoader.FreeTypeFontLoaderParameter parameter120 = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
         parameter120.fontFileName = "Roboto-Light.ttf";
-        parameter120.fontParameters.size = (int)(120 * (BaseScreen.SCALE_X));
+        parameter120.fontParameters.size = Math.round(120 * (BaseScreen.SCALE_X));
         parameter120.fontParameters.color = Color.WHITE;
         game.assetManager.load("font120.ttf", BitmapFont.class, parameter120);
     }
